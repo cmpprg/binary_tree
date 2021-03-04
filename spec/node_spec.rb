dd5f1cfd32b3c790c8f0a6de77d5598ff1a946fd
Ryan Camp <campryan@comcast.net>
2021-03-03 18:32:56 -0700

    Write test to check that score and movie title attr readable

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,10 +2,19 @@ require '../lib/node'
 
 RSpec.describe Node do
   it 'exists' do
-    value = 5
+    score = 5
     movie_title = "Johnny English"
-    node = Node.new(value, movie_title)
+    node = Node.new(score, movie_title)
 
     expect(node).to be_an_instance_of(Node)
+  end
+
+  it 'has attribtutes' do
+    score = 5
+    movie_title = "Johnny English"
+    node = Node.new(score, movie_title)
+
+    expect(node.score).to eql(score)
+    expect(node.movie_title).to eql(movie_title)
   end
 end
